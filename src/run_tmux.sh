413d2f3affed85cfc1bca2b4e7316f1d5c8bba1e
Andy Hall <ahall@rti.com>
2024-12-04 14:37:12 +0100

    Removed unnecessary, and case corrected comments

--- src/run_tmux.sh
+++ src/run_tmux.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# Set Session Name
+# Set session name
 SESSION="OtelAdapter"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
 # Only create tmux session if it doesn't already exist
 if [ "$SESSIONEXISTS" = "" ]
 then
-    # Start New Session with our name
+    # Start new session with our name
     tmux new-session -d -s $SESSION
 
-    # Name first Pane and start the routing service
+    # Name first pane and start the routing service
     tmux rename-window -t 0 'Routing Service'
     tmux send-keys -t 'Routing Service' 'rtiroutingservice -cfgFile ./RsTelemetryGateway.xml -cfgName "OpenTelemetryGateway"' C-m 'clear' C-m
 
@@ -19,5 +19,5 @@ then
     tmux send-keys -t 'Metrics Generator' './generator -d 2 -q generator_qos.xml' C-m
 fi
 
-# Attach Session, on the Main window
+# Attach session, on the main window
 tmux attach-session -t $SESSION:0
